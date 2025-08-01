2ee5ecbb95a6485f226d782068c6f794f181ef64
May May <thepullinger@gmail.com>
2025-08-01 11:34:21 +0100

    update event handler to use custom inputs

--- src/event_handler.gd
+++ src/event_handler.gd
@@ -6,13 +6,13 @@ var player: Entity
 func get_action() -> Action:
 	var action: Action = null
 
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_just_pressed("move_up"):
 		action = BumpAction.new(player, 0, -1)
-	elif Input.is_action_just_pressed("ui_down"):
+	elif Input.is_action_just_pressed("move_down"):
 		action = BumpAction.new(player, 0, 1)
-	elif Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_just_pressed("move_left"):
 		action = BumpAction.new(player, -1, 0)
-	elif Input.is_action_just_pressed("ui_right"):
+	elif Input.is_action_just_pressed("move_right"):
 		action = BumpAction.new(player, 1, 0)
 
 	return action
