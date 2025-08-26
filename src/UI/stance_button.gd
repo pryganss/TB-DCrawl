eadf410f6e793b3f57ac2f378574ba25ff264c1d
May May <thepullinger@gmail.com>
2025-08-26 15:35:58 +0100

    add ui scene for stance changing

---
+++ src/UI/stance_button.gd
@@ -0,0 +1,16 @@
+extends Button
+
+var offered_stance: StanceDefinition
+
+func change_offered_stance(stance_definition: StanceDefinition):
+	offered_stance = stance_definition
+
+	%Title.text = offered_stance.name
+	%Description.text = offered_stance.description
+
+func _on_pressed() -> void:
+	var fighter_component = Map.player.components.get(cpnt.FIGHTER) as FighterComponent
+	if fighter_component.stance:
+		fighter_component.stance.change_stance(offered_stance)
+	else:
+		fighter_component.stance = Stance.new(offered_stance, Map.player)
