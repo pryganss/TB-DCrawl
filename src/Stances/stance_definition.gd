1c83f31c70dc3cf0710fb4439aa4118ef2639aac
May May <thepullinger@gmail.com>
2025-08-26 15:28:38 +0100

    add extra information to stance definition

--- src/Stances/stance_definition.gd
+++ src/Stances/stance_definition.gd
@@ -2,3 +2,7 @@ class_name StanceDefinition
 extends Resource
 
 @export var status: Array[StatusDefinition]
+
+@export_category("UI")
+@export var name: String = "unnamed_stance"
+@export_multiline var description: String = "No Description"
