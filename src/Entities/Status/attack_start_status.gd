9c3992bc4538618798e7461bcef1450004839555
may <thepullinger@gmail.com>
2025-08-28 13:59:24 +0100

    remove redundant constructors

--- src/Entities/Status/attack_start_status.gd
+++ src/Entities/Status/attack_start_status.gd
@@ -1,9 +1,6 @@
 class_name OnAttackStartStatus
 extends Status
 
-func _init(affected_entity: Entity, start_duration):
-	super._init(affected_entity, start_duration)
-
 func _get_triggers():
 	var melee_component = entity.components.get(cpnt.MELEE) as MeleeComponent
 	return [melee_component.attack_started]
--- src/Entities/Status/on_hit_status.gd
+++ src/Entities/Status/on_hit_status.gd
@@ -1,9 +1,6 @@
 class_name OnHitStatus
 extends Status
 
-func _init(affected_entity: Entity, start_duration: int):
-	super._init(affected_entity, start_duration)
-
 func _get_triggers():
 	var fighter_component = entity.components.get(cpnt.FIGHTER) as FighterComponent
 	return [fighter_component.damage_started]
