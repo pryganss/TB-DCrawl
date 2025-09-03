aa2ea22d0389d6c84c2a550af9af4b7197288314
May May <thepullinger@gmail.com>
2025-09-03 13:24:09 +0100

    rebalance sword cleaver status

--- src/Entities/Status/armor_from_damage_status.gd
+++ src/Entities/Status/armor_from_damage_status.gd
@@ -1,15 +1,16 @@
 class_name ArmorFromDamageStatus
 extends OnHitStatus
 
-var last_attack_damage = 0
+var last_attack_damage = 0.0
 
 func apply(args: Dictionary, signal_name: String = "unnamed_signal"):
 	if signal_name == "attack_ended":
 		if not args["damage"] == 0:
-			last_attack_damage = args["damage"]
+			last_attack_damage = float(args["damage"])
 	else:
 		var fighter_component = entity.components.get(cpnt.FIGHTER) as FighterComponent
-		fighter_component.armor += ceili(last_attack_damage * 1.5)
+		var melee_component = entity.components.get(cpnt.MELEE) as MeleeComponent
+		fighter_component.armor += ceili(last_attack_damage * (0.25 + (float(melee_component.BASE_DELAY) / 100.0)))
 
 func _get_triggers():
 	var melee_component = entity.components.get(cpnt.MELEE) as MeleeComponent
