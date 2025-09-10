399036829f9d6c14b0a3e92696eeeb074903144c
May May <thepullinger@gmail.com>
2025-09-10 14:17:26 +0100

    remove unneeded check from dot_status

--- src/Entities/Status/dot_status.gd
+++ src/Entities/Status/dot_status.gd
@@ -6,8 +6,7 @@ var counter: int = 100
 func apply(_args = {}, _signal_name = "unnamed_signal"):
 	if counter >= 100:
 		var fighter_component: FighterComponent = entity.components.get(cpnt.FIGHTER) as FighterComponent
-		if fighter_component:
-			fighter_component.damage(1, Color.GREEN, true)
+		fighter_component.damage(1, Color.GREEN, true)
 		counter -= 100
 
 	counter += 1
